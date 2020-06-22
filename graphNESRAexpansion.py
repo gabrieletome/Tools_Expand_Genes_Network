1a69f576149ecc9231ed8180434e83679a134a0a
gabrieletome <39188419+gabrieletome@users.noreply.github.com>
2020-06-22 16:13:57 +0200

    Upload drawing NESRA expansion

--- graphNESRAexpansion.py
+++ graphNESRAexpansion.py
@@ -24,16 +24,44 @@ def readFiles(filename):
         sys.exit(-1)
     #split rows
     listLine = text.split('\n')
-    listTuples = [listLine[0].split(' ')[3]]
-    i = 2
+    #listTuples = [listLine[0].split(' ')[3]]
+    listTuples = [listLine[0].split('_')[0]] #TODEL
+    i = 1
     while i < len(listLine):
         try:
-            if listLine[i] != '':
-                listTuples.append((int(listLine[i].split(',')[0]), listLine[i].split(',')[1].upper(), listLine[i].split(',')[2].upper(), float(listLine[i].split(',')[4]), bool(listLine[i].split(',')[5])))
+            if listLine[i] != '' and listLine[i].split(',')[0] == 'GT-001': #TODEL GT-001
+                #listTuples.append((int(listLine[i].split(',')[0]), listLine[i].split(',')[1].upper(), listLine[i].split(',')[2].upper(), float(listLine[i].split(',')[4]), bool(listLine[i].split(',')[5])))
+                listTuples.append((listLine[i].split(',')[0], int(listLine[i].split(',')[1]), listLine[i].split(',')[2], float(listLine[i].split(',')[3]))) #TODEL
         except:
             pass
         i += 1
     return listTuples
+
+#read file
+def readFilesInt(filename):
+    print('Open file: '+filename, flush=True)
+    try:
+        f = open(filename, 'rU')
+        text = f.read()
+        f.close()
+    except:
+        #except if file does not exist
+        print('ERROR: FILE NOT FOUND. File \''+filename+'\' does not exist')
+        sys.exit(-1)
+    #split rows
+    listLine = text.split('\n')
+    listTuples = []
+    i = 1
+    while i < len(listLine):
+        try:
+            if listLine[i] != '': #TODEL GT-001
+                listTuples.append((int(listLine[i].split(',')[0]), listLine[i].split(',')[1].upper(), listLine[i].split(',')[2].upper(), float(listLine[i].split(',')[4]), (listLine[i].split(',')[5])))
+                #listTuples.append((listLine[i].split(',')[0].upper(), int(listLine[i].split(',')[1]), listLine[i].split(',')[2].upper(), float(listLine[i].split(',')[3]))) #TODEL
+        except:
+            pass
+        i += 1
+    return listTuples
+
 
 #Function to read the update name of vitis genes
 def updateNameVitis():
@@ -89,20 +117,30 @@ def ord2(tuple):
 def main():
     if len(sys.argv) >= 2:
         #build matrix of genes
-        listTuple = manageBR(readFiles(sys.argv[-1]), 1, 2)
+        #listTuple = manageBR(readFiles(sys.argv[-1]), 1, 2)
+        listTuple = readFiles(sys.argv[-2]) #TODEL
         listTuple = [listTuple[0]]+sorted(listTuple[1:], key=ord2)
         updateNameVitis()
         #add filter
-        filter = sys.argv[1:-1]
+        filter = sys.argv[1:-2]
         if filter[0] == '-frel':
-            listTuple = [listTuple[0]]+[(a,b,c,d,e) for (a,b,c,d,e) in listTuple[1:] if d > float(filter[1])]
+            #listTuple = [listTuple[0]]+[(a,b,c,d,e) for (a,b,c,d,e) in listTuple[1:] if d > float(filter[1])]
+            listTuple = [listTuple[0]]+[(a,b,c,d) for (a,b,c,d) in listTuple[1:] if d > float(filter[1])] #TODEL
         elif filter[0] == '-rank':
             listTuple = [listTuple[0]]+[(a,b,c,d,e) for (a,b,c,d,e) in listTuple[1:] if a < int(filter[1])]
-        edges = [['MYB141','VvERF042','VvERF043','VvERF044']]+[(listBioNameUpdate[u[1]], u[0], listBioNameUpdate[u[2]], u[3]) for u in listTuple[1:]]
+        #edges = [['MYB141','VvERF042','VvERF043','VvERF044']]+[(listBioNameUpdate[u[1]], u[0], listBioNameUpdate[u[2]], u[3]) for u in listTuple[1:]]
+        edges = [['GT-001']]+[(u[0], u[1], u[2], u[3]) for u in listTuple[1:]]
+        #read file interaction
+        listTupleInt = [(u[0], listBioNameUpdate[u[1]], listBioNameUpdate[u[2]], u[3], u[4]) for u in manageBR(readFilesInt(sys.argv[-1]), 1, 2)] #TODEL
+        edgesInt = [['MYB141','VvERF042','VvERF043','VvERF044']]+[(u[2], u[0], u[1], u[3]) for u in listTupleInt if u[4] == 'True' or u[1] in [k[2] for k in edges[1:]]]+[(u[1], u[0], u[2], u[3]) for u in listTupleInt if u[2] in [x[2] for x in edges[1:]]]
+        edgesInt = [edgesInt[0]]+[(a,b,c,d) for (a,b,c,d) in edgesInt[1:] if d > float(filter[1])]
+        print(edgesInt)
+        
         pearsonComplete = []
-        listForPearson = [(b,c,d) for (a,b,c,d,e) in listTuple[1:]]
-        tmp = utex.manageBR(ut.pearsonCorrelation(listForPearson, 'vv_exprdata_2.csv'))
-        pearson = [(listBioNameUpdate[u], listBioNameUpdate[v],p) for (u,v,p) in manageBR(tmp, 0, 1)]
+        # listForPearson = [(b,c,d) for (a,b,c,d,e) in listTuple[1:]]
+        # tmp = utex.manageBR(ut.pearsonCorrelation(listForPearson, 'vv_exprdata_2.csv'))
+        #pearson = [(listBioNameUpdate[u], listBioNameUpdate[v],p) for (u,v,p) in manageBR(tmp, 0, 1)]
+        pearson = [(u[0], u[2], 1) for u in edgesInt[1:]]
         pearsonComplete.append(pearson)
         #create directory
         if not os.path.exists('commonGenesOutput'):
@@ -111,12 +149,12 @@ def main():
         time = dt.datetime.now()
         printTime = time.strftime("%Y%m%d%H%M%S")
         os.mkdir('commonGenesOutput/'+printTime)
-        os.mkdir('commonGenesOutput/'+printTime+'/'+utex.buildNamefile(edges))
-        os.mkdir('commonGenesOutput/'+printTime+'/'+utex.buildNamefile(edges)+'/'+utex.buildNamefile(edges))
+        os.mkdir('commonGenesOutput/'+printTime+'/'+utex.buildNamefile(edgesInt))
+        os.mkdir('commonGenesOutput/'+printTime+'/'+utex.buildNamefile(edgesInt)+'/'+utex.buildNamefile(edgesInt))
         global nameDir
-        nameDir = 'commonGenesOutput/'+printTime+'/'+utex.buildNamefile(edges)+'/'
+        nameDir = 'commonGenesOutput/'+printTime+'/'+utex.buildNamefile(edgesInt)+'/'
         print('Creating directory: \''+nameDir+'\'', flush=True)
-        graphic.printCommonGraph([edges], pearsonComplete, 1-round((edges[-1][3]), 1), nameDir, False)
+        graphic.printCommonGraph([edgesInt], pearsonComplete, 1-round((edges[-1][3]), 1), nameDir, True, listBioNameUpdate)
 
 #Calls the main() function.
 if __name__ == '__main__':
